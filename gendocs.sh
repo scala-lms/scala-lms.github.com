7fe289e7b6b8971fa80aed12c30363457049b325
Tiark Rompf <tiark.rompf@epfl.ch>
2014-07-25 18:07:50 +0200

    generated tutorial html

--- gendocs.sh
+++ gendocs.sh
@@ -5,5 +5,8 @@
 #   summer-of-lms-2014
 #
 
-(cd ../scalocco/; sbt "run ../scala-lms-tutorials-new/src/test/scala/lms/tutorial/ ../summer-of-lms-2014/tutorials/ ../summer-of-lms-2014/_layouts/code.html")
+DIR=`(cd ..; pwd)`
+ARGS="$DIR/scala-lms-tutorials-new/src/test/scala/lms/tutorial/ $DIR/summer-of-lms-2014/tutorials/ $DIR/summer-of-lms-2014/_layouts/code.html"
+
+(cd ../scalocco/; sbt "run $ARGS")
 
