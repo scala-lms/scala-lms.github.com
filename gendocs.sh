f8d5dc028d987f97314871aa2d648e1eaea93f4f
Tiark Rompf <tiark.rompf@epfl.ch>
2014-08-25 20:02:13 -0400

    update base url

--- gendocs.sh
+++ gendocs.sh
@@ -1,12 +1,12 @@
 # requires the following directories side-by-side:
 #
 #   scalocco                  https://github.com/tiarkrompf/scalocco
-#   scala-lms-tutorials-new   https://github.com/scala-lms/tutorials branch markdownized
+#   scala-lms-tutorials       https://github.com/scala-lms/tutorials
 #   summer-of-lms-2014        this repo
 #
 
 DIR=`(cd ..; pwd)`
-ARGS="$DIR/scala-lms-tutorials-new/src/test/scala/lms/tutorial/ $DIR/summer-of-lms-2014/tutorials/ $DIR/summer-of-lms-2014/_layouts/code.html"
+ARGS="$DIR/scala-lms-tutorials/src/test/scala/lms/tutorial/ $DIR/summer-of-lms-2014/tutorials/ $DIR/summer-of-lms-2014/_layouts/code.html"
 
 (cd ../scalocco/; sbt "run $ARGS")
 
