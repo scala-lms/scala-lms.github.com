5f24cb2a0a591b44fd7ed7a7eebdc7fdbd3de62c
Ruben Fiszel <ruben.fiszel@epfl.ch>
2016-09-02 19:07:41 -0700

    color + latex

--- gendocs.sh
+++ gendocs.sh
@@ -6,7 +6,6 @@
 #
 
 DIR=`(cd ..; pwd)`
-ARGS="$DIR/scala-lms-tutorials/src/test/scala/lms/tutorial/ $DIR/scala-lms.github.com/tutorials/ $DIR/scala-lms.github.com/_layouts/code.html"
+ARGS="$DIR/tutorials/src/test/scala/lms/tutorial/ $DIR/scala-lms.github.com/tutorials/ $DIR/scala-lms.github.com/_layouts/code.html"
 
 (cd ../scalocco/; sbt "run $ARGS")
-
